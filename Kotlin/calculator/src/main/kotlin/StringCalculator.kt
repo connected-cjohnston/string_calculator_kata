85903f8f34b0b7288446deaab77e12f653be5f6e
Chris Johnston <cjohnston@connected.io>
2023-10-16 11:34:20 -0400

    Refactors add method to extract delimiters and methods

--- Kotlin/calculator/src/main/kotlin/StringCalculator.kt
+++ Kotlin/calculator/src/main/kotlin/StringCalculator.kt
@@ -1,24 +1,44 @@
 class StringCalculator {
+
     fun add(str: String): Int {
         if (str == "") return 0
 
-        var ints: List<Int>? = null
+        return validInts(str).sumOf { it.toInt() }
+    }
 
-        ints = if (str.contains("//")) {
-            val parts = str.replace("//", "").split("\n")
-            parts[1].split(parts[0]).map { it.toInt() }
-        } else {
-            val intStrings = str.split(",", "\n")
-            intStrings.map { it.toInt() }
-        }
+    private fun validInts(str: String): List<Int> {
+        val ints: List<Int> = Delimiter.create(str).split().map { it.toInt() }
+        ensurePositiveIntegers(ints)
+        return excludeLargeInts(ints)
+    }
 
-        val negatives = ints.filter { it < 0 }
+    private fun ensurePositiveIntegers(ints: List<Int>) {
+        val negatives: List<Int> = ints.filter { it < 0 }
         if (negatives.isNotEmpty()) {
             throw Exception(
                 "negatives not allowed: ${negatives.joinToString(",")}"
             )
         }
+    }
 
-        return ints.filter { it <= 1000 }.sumOf { it.toInt() }
+    private fun excludeLargeInts(ints: List<Int>) = ints.filter { it <= 1000 }
+}
+
+open class Delimiter(private val str: String) {
+    open fun split() = str.split(",", "\n")
+
+    companion object {
+        fun create(str: String) = if (str.contains("//")) {
+            CustomDelimiter(str)
+        } else {
+            Delimiter(str)
+        }
+    }
+}
+
+class CustomDelimiter(private val str: String) : Delimiter(str) {
+    override fun split(): List<String> {
+        val parts = str.replace("//", "").split("\n")
+        return parts[1].split(parts[0])
     }
 }
